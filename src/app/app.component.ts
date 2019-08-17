70eac4aeaf575c500c377d35b251ccce8d4ba954
Gorgant CX1 <gorgant@gorgant.com>
2019-08-17 20:27:28 +0300

    update to 1.1.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { withLatestFrom } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   title = 'Admin - Mary Daphne';
-  appVersion = '1.1.3';
+  appVersion = '1.1.4';
 
   @ViewChild('sidenav', { static: true }) sidenav: MatSidenav;
 
